9e52400b8b2061b5be5d3b0faaa650ee21a0b79b
Steve  Tran <stevetran@RESNET-ERC.local>
2018-07-24 22:15:15 -0700

    Added Angular Material

--- episode-app/src/app/app.module.ts
+++ episode-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatButtonModule, MatInputModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { MovieItemComponent } from './movie/movie-item/movie-item.component';
@@ -7,6 +8,7 @@ import { SearchBarComponent } from './search/search-bar/search-bar.component';
 import { MovieListComponent } from './movie/movie-list/movie-list.component';
 import { TvSeriesListComponent } from './tv-series/tv-series-list/tv-series-list.component';
 import { TvSeriesItemComponent } from './tv-series/tv-series-item/tv-series-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -19,7 +21,11 @@ import { TvSeriesItemComponent } from './tv-series/tv-series-item/tv-series-item
     TvSeriesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
