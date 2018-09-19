50069ffb272a9764c9d4a8da1122eb7483c934d3
Steve Tran <stran2252@gmail.com>
2018-09-19 15:51:26 -0700

    Nullify token on logout and update UI

--- episode-app/src/app/header/header.component.ts
+++ episode-app/src/app/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Subscription } from '../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isUserAuthenticated = false;
+  private authListenerSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authListenerSubscription = this.authService.getAuthStatusListener().subscribe((isAuthenticated) =>{
+      this.isUserAuthenticated = isAuthenticated;
+    });
+  }
+
+  ngOnDestroy() {
+    this.authListenerSubscription.unsubscribe();
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 
 }
--- episode-app/src/app/services/auth.service.ts
+++ episode-app/src/app/services/auth.service.ts
@@ -2,11 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { AuthData } from '../models/auth-data.model';
+import { Subject } from '../../../node_modules/rxjs';
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
   private token: string;
+  private authStatusListener = new Subject<boolean>();
+  private isAuthenticated = false;
+
   constructor(private http: HttpClient) {}
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
+
+  getIsAuthenticated() {
+    return this.isAuthenticated;
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
@@ -20,7 +32,17 @@ export class AuthService {
     this.http.post<{message: string, token: string}>('http://localhost:3000/api/user/login', authData).subscribe(response => {
       const token = response.token;
       this.token = token;
+      if (token) {
+        this.isAuthenticated = true;
+        this.authStatusListener.next(true);
+      }
     });
+  }
+
+  logout() {
+    this.token = null;
+    this.isAuthenticated = false;
+    this.authStatusListener.next(false);
   }
 
   getToken() {
